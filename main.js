f49c28dd9f454692e2fb3f2e42c3b7fbd89fa5eb
Jarrett Huang <jarethuang@gmail.com>
2018-12-03 12:28:46 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-  particlesJS.load('particles-js', 'particles.json', function() {
-     console.log('callback - particles.js config loaded');
+  particlesJS.load('particles-js', 'particles.json', function() {});
 });
