0c79f2fc0fac8cb0d0148e72f4fc0060f2f7e1fa
Jarrett Huang <jarethuang@gmail.com>
2018-12-04 00:02:58 -0800

    Ravamped look and added google sheets for database

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
+    var random_boolean = Math.random() >= 0.5;
+    if (random_boolean) {
+        $("body").css("background-color", "white");
+        $("body").css("filter", "invert(100%)");
+    }
+    
+    $("#join-button").click(function() {
+        $("#application-form").modal({
+            fadeDuration: 100
+        });
+    });
 });
