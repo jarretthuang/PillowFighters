5e928f9d217f4da71b739835183b61ffd5257fd5
Jarrett Huang <jarethuang@gmail.com>
2018-12-04 01:24:25 -0800

    Added api parser

--- main.js
+++ main.js
@@ -10,4 +10,37 @@ $(document).ready(function(){
             fadeDuration: 100
         });
     });
+    
+    $("#ign-input").focusout(function() {
+        jsonParser($("#ign-input").val());
+    });
+    
+    function jsonParser(name) {
+        var url = "https://api.silveress.ie/bns/v3/character/full/na/" + name;
+        $.getJSON(url, function(data) {
+            if (isEmpty(data)) {
+                $("#char-lookup").html("Cannot find you on NA server!");
+            } else {
+                $("#char-lookup").html(confirmationText(data));
+            }
+            showCharacterLookup();
+        });
+    }
+    
+    function showCharacterLookup() {
+        $("#char-lookup").show();
+    }
+    
+    function isEmpty(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+            return false;
+        }
+        return true;
+    }
+    
+    function confirmationText(char) {
+        return "LV" + char.playerLevel + " HM" + char.playerLevelHM + " " + char.playerClass + " is that you?";
+    }
+    
 });
